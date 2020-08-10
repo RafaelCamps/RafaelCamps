c7a81acaa4649dadc2457542e00fd9c8235e75ab
Rafael Camps <rcamps@cifpfbmoll.eu>
2020-08-10 20:10:00 +0200

    Prueba crear index.html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rafael Camps</title>
+</head>
+<body>
+    <h1>Esta es la Web personal de Rafael Camps</h1>
+</body>
+</html>
